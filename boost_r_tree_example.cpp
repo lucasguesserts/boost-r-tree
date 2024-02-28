19dac096edf57849b9b3d408c9cede5560b6467f
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2024-02-28 15:17:46 -0300

    feat: use custom data structures

--- boost_r_tree_example.cpp
+++ boost_r_tree_example.cpp
@@ -7,7 +7,11 @@
 #include <boost/geometry/geometries/box.hpp>
 #include <boost/geometry/geometries/point.hpp>
 
+#include <boost/geometry/index/predicates.hpp>
 #include <boost/geometry/index/rtree.hpp>
+
+#include <boost/geometry/geometries/register/point.hpp>
+#include <boost/geometry/geometries/register/box.hpp>
 
 // to store queries results
 #include <cstddef>
@@ -19,10 +23,68 @@
 namespace bg = boost::geometry;
 namespace bgi = boost::geometry::index;
 
-using Point = bg::model::point<double, 3, bg::cs::cartesian>;
-using Box = bg::model::box<Point>;
+// CHOOSE ONE OF THE FOLLOWING (UNCOMMENT ONE AND COMMENT THE OTHER)
+// 1. Boost Data Structures
+// 2. Custom Data Structures
+
+// // ------------------------------------------------------------
+// // Boost Data Structures
+// // ------------------------------------------------------------
+
+// using Point = bg::model::point<double, 3, bg::cs::cartesian>;
+// using Box = bg::model::box<Point>;
+// using Value = std::pair<Box, unsigned>;
+// using RTree = bgi::rtree<Value, bgi::quadratic<16>>;
+
+// ------------------------------------------------------------
+// Custom Data Structures
+// ------------------------------------------------------------
+
+class Point {
+public:
+    Point(double x, double y, double z)
+        : _x(x)
+        , _y(y)
+        , _z(z) {}
+
+    // set
+    double & x() { return _x; }
+    double & y() { return _y; }
+    double & z() { return _z; }
+
+    // get
+    const double & x() const { return _x; }
+    const double & y() const { return _y; }
+    const double & z() const { return _z; }
+private:
+    double _x;
+    double _y;
+    double _z;
+};
+BOOST_GEOMETRY_REGISTER_POINT_3D(Point, double, bg::cs::cartesian, x(), y(), z());
+// BOOST_GEOMETRY_REGISTER_POINT_3D_CONST(Point, double, bg::cs::cartesian, x(), y(), z()); // using this, the 'set' methods can be removed
+
+class Box {
+public:
+    Box(const Point & min, const Point & max)
+        : _min(min)
+        , _max(max) {}
+
+    const Point & min() const { return _min; }
+    const Point & max() const { return _max; }
+private:
+    Point _min;
+    Point _max;
+};
+BOOST_GEOMETRY_REGISTER_BOX(Box, Point, min(), max());
+
 using Value = std::pair<Box, unsigned>;
+
 using RTree = bgi::rtree<Value, bgi::quadratic<16>>;
+
+// ------------------------------------------------------------
+// Main
+// ------------------------------------------------------------
 
 int main() {
 
@@ -41,7 +103,7 @@ int main() {
         Box{Point(5, 5, 5), Point(5 + 4, 5 + 4, 5 + 4)},
     };
     for (size_t i = 0; i < boxes.size(); ++i) {
-        rtree.insert(std::make_pair(boxes[i], i));
+        rtree.insert(Value(boxes[i], i));
     }
 
     // Print all rectangles in the R-tree
@@ -54,22 +116,19 @@ int main() {
     // find values intersecting some area defined by a Box
     Box query_box(Point(3, 3, 4), Point(3 + 4, 3 + 4, 4 + 6));
     std::vector<Value> result_s;
-    auto valid_intersection = [&query_box](const Value & v) {
-        return !bg::touches(query_box, v.first);
-    };
     rtree.query(
-        bgi::intersects(query_box) && bgi::satisfies(valid_intersection),
+        bgi::overlaps(query_box),
         std::back_inserter(result_s));
 
     // display results
     std::cout << std::endl;
-    std::cout << "spatial query Box:" << std::endl;
+    std::cout << "spatial query BoostBox:" << std::endl;
     std::cout << bg::dsv(query_box) << std::endl;
     std::cout << std::endl;
     std::cout << "spatial query result:" << std::endl;
-    for (Value const & v : result_s) {
+    for (auto const & v : result_s) {
         std::cout << bg::dsv(v.first) << " - " << v.second << std::endl;
-        // std::cout << bg::wkt<Box>(v.first) << " - " << v.second << std::endl;
+        // std::cout << bg::wkt<BoostBox>(v.first) << " - " << v.second << std::endl;
     }
 
     return 0;
