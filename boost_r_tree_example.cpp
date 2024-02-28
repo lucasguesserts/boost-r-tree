cabfe77278e67b5e9e8332ea6a584d4da634d4b7
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2024-02-28 14:47:12 -0300

    feat: make example work with three-dimensional boxes

--- boost_r_tree_example.cpp
+++ boost_r_tree_example.cpp
@@ -2,12 +2,15 @@
 // https://www.boost.org/doc/libs/1_84_0/libs/geometry/doc/html/geometry/spatial_indexes/rtree_quickstart.html
 
 #include <boost/geometry.hpp>
+#include <boost/geometry/algorithms/detail/touches/interface.hpp>
+#include <boost/geometry/algorithms/is_empty.hpp>
 #include <boost/geometry/geometries/box.hpp>
 #include <boost/geometry/geometries/point.hpp>
 
 #include <boost/geometry/index/rtree.hpp>
 
 // to store queries results
+#include <cstddef>
 #include <vector>
 
 // just for output
@@ -16,7 +19,7 @@
 namespace bg = boost::geometry;
 namespace bgi = boost::geometry::index;
 
-using Point = bg::model::point<double, 2, bg::cs::cartesian>;
+using Point = bg::model::point<double, 3, bg::cs::cartesian>;
 using Box = bg::model::box<Point>;
 using Value = std::pair<Box, unsigned>;
 using RTree = bgi::rtree<Value, bgi::quadratic<16>>;
@@ -26,14 +29,19 @@ int main() {
     // create the rtree using default constructor
     RTree rtree;
 
-    // create some values
-    for (unsigned i = 0; i < 10; ++i) {
-        // create a Box
-        Box b(
-            Point(i + 0.0, i + 0.0),
-            Point(i + 2.0, i + 2.0));
-        // insert new Value
-        rtree.insert(std::make_pair(b, i));
+    // add 8 boxes
+    const auto boxes = std::vector{
+        Box{Point(0, 0, 0), Point(0 + 4, 0 + 4, 0 + 4)},
+        Box{Point(5, 0, 0), Point(5 + 4, 0 + 4, 0 + 4)},
+        Box{Point(0, 5, 0), Point(0 + 4, 5 + 4, 0 + 4)},
+        Box{Point(5, 5, 0), Point(5 + 4, 5 + 4, 0 + 4)},
+        Box{Point(0, 0, 5), Point(0 + 4, 0 + 4, 5 + 4)},
+        Box{Point(5, 0, 5), Point(5 + 4, 0 + 4, 5 + 4)},
+        Box{Point(0, 5, 5), Point(0 + 4, 5 + 4, 5 + 4)},
+        Box{Point(5, 5, 5), Point(5 + 4, 5 + 4, 5 + 4)},
+    };
+    for (size_t i = 0; i < boxes.size(); ++i) {
+        rtree.insert(std::make_pair(boxes[i], i));
     }
 
     // Print all rectangles in the R-tree
@@ -44,14 +52,14 @@ int main() {
     }
 
     // find values intersecting some area defined by a Box
-    Box query_box(Point(1, 1), Point(3, 3));
+    Box query_box(Point(3, 3, 4), Point(3 + 4, 3 + 4, 4 + 6));
     std::vector<Value> result_s;
-    auto has_area_intersection = [&query_box](const Value& v) {
-        Box R;
-        bg::intersection(v.first, query_box, R);
-        return bg::area(R) > 0;
+    auto valid_intersection = [&query_box](const Value & v) {
+        return !bg::touches(query_box, v.first);
     };
-    rtree.query(bgi::intersects(query_box) && bgi::satisfies(has_area_intersection), std::back_inserter(result_s));
+    rtree.query(
+        bgi::intersects(query_box) && bgi::satisfies(valid_intersection),
+        std::back_inserter(result_s));
 
     // display results
     std::cout << std::endl;
